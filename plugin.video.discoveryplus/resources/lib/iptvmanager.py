19cea4b60f21faedb0a93489a63172c9b9158661
Dis90 <suominen90@gmail.com>
2021-12-18 23:17:46 +0200

    plugin.video.discoveryplus v.1.6.5

--- plugin.video.discoveryplus/resources/lib/iptvmanager.py
+++ plugin.video.discoveryplus/resources/lib/iptvmanager.py
@@ -35,9 +35,10 @@ class IPTVManager:
     @via_socket
     def send_channels(self):
         """Return JSON-STREAMS formatted python datastructure to IPTV Manager"""
-        if helper.d.locale_suffix == 'us':
+        # go = US and Canada
+        if helper.d.realm == 'go':
             streams = helper.d.get_channels_us()
-        elif helper.d.locale_suffix == 'in':
+        elif helper.d.realm == 'dplusindia':
             streams = helper.d.get_channels_in()
         else:
             streams = helper.d.get_channels()
@@ -47,9 +48,10 @@ class IPTVManager:
     @via_socket
     def send_epg(self):
         """Return JSON-EPG formatted python data structure to IPTV Manager"""
-        if helper.d.locale_suffix == 'us':
+        # go = US and Canada
+        if helper.d.realm == 'go':
             epg = helper.d.get_epg_us()
-        elif helper.d.locale_suffix == 'in':
+        elif helper.d.realm == 'dplusindia':
             epg = helper.d.get_epg_in()
         else:
             epg = helper.d.get_epg()
